cb39cf0b8cd0a37aeb44ecc3115abe676eee750f
SANS-1104 <sanskriti020803@gmail.com>
2024-07-10 22:35:24 +0530

    commit 8

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,22 +11,20 @@ export default function Navbar(){
         navRef.current.classList.toggle('responsive_nav')
     }
     return(
-        <nav className="navbar header w-full flex flex-wrap justify-between items-center fixed z-50 mt-0">
-            <div className="navbar-container main-logo">
-                <img src={logo} alt="Logo" />
-            </div>
-            <div className="links mr-20 flex-1 flex flex-wrap items-center justify-evenly" ref={navRef}>
-                <a href="#about" className=''>About</a>
-                <a href="#services" className=''>Services</a>
-                <a href="#team" className=''>Team</a>
-                <a href="#contact" className=''>Contact Us</a>
+        <header>
+            <img src={logo} alt="Logo" className='logo'/>
+            <nav ref={navRef}>
+                <a href="#about">About</a>
+                <a href="#services">Services</a>
+                <a href="#team">Team</a>
+                <a href="#contact">Contact Us</a>
                 <button onClick={showNavBar} className='nav-btn nav-close-btn'>
                     <FaTimes />
                 </button>
-            </div>
+            </nav>
             <button onClick={showNavBar} className='nav-btn'>
                 <FaBars />
             </button>
-        </nav>
+        </header>
     )
 }
