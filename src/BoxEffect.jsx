77d48ef53bcd862293040a2af9301263ffb5d432
SANS-1104 <sanskriti020803@gmail.com>
2024-07-11 17:31:35 +0530

    commit 12

--- src/BoxEffect.jsx
+++ src/BoxEffect.jsx
@@ -6,7 +6,7 @@ import './About.css'
 
 export default function BackgroundBoxesDemo() {
   return (
-    <div className="h-96 relative w-full mb-24 overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+    <div className="h-96 relative w-full mb-24 overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg" id="contact">
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
       <Boxes />
